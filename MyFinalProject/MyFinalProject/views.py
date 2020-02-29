e78a423cedaf917b1cdc3cb2d57e29b3860e7364
Oran-Uzrad <50901272+Oran-Uzrad@users.noreply.github.com>
2020-02-29 17:32:56 +0200

    2/29/20 Data, About, Contact

--- MyFinalProject/MyFinalProject/views.py
+++ MyFinalProject/MyFinalProject/views.py
@@ -36,7 +36,8 @@ def home():
     return render_template(
         'index.html',
         title='Home Page',
-        year=datetime.now().year,
+        img_american_flag = '/static/imgs/american_flag.jpg',
+        year=datetime.now().year
     )
 
 @app.route('/contact')
@@ -48,9 +49,9 @@ def contact():
 
     return render_template(
         'contact.html',
-        title='Contact',
         year=datetime.now().year,
-        message='Your contact page.'
+        img_tichonet = '/static/imgs/tichonet.png',
+        img_oran = '/static/imgs/oran.jpg'
     )
 
 @app.route('/about')
@@ -61,9 +62,8 @@ def about():
     """Renders the about page."""
     return render_template(
         'about.html',
-        title='About',
         year=datetime.now().year,
-        message='Your application description page.'
+        img_tichonet = '/static/imgs/tichonet.png'
     )
 
 @app.route('/data')
@@ -77,7 +77,7 @@ def data():
         title='Data',
         year=datetime.now().year,
         message='My data page.',
-        img_trump = '/static/imgs/obama.jpg',
+        img_trump = '/static/imgs/trump.jpg',
         img_obama = '/static/imgs/obama.jpg',
         img_bush = '/static/imgs/bush.jpg',
         img_clinton = '/static/imgs/clinton.jpg'
@@ -124,7 +124,7 @@ def trump():
         'trump.html',
         title='Trump',
         year=datetime.now().year,
-        message='My Trump page.',
+        message='Trump dataset page.',
         img_trump = '/static/imgs/trump.jpg',
         img_obama = '/static/imgs/trump.jpg',
         img_bush = '/static/imgs/trump.jpg',
@@ -132,6 +132,117 @@ def trump():
         raw_data_table = raw_data_table,
         form1 = form1,
         form2 = form2
+    )
+
+@app.route('/data/obama' , methods = ['GET' , 'POST'])
+def obama():
+
+    print("Obama")
+
+    """Renders the about page."""
+    form1 = ExpandForm()
+    form2 = CollapseForm()
+    # df = pd.read_csv(path.join(path.dirname(__file__), 'static\\data\\trump.csv'))
+    df = pd.read_csv(path.join(path.dirname(__file__), 'static/data/obama.csv'))
+    raw_data_table = ''
+
+    if request.method == 'POST':
+        if request.form['action'] == 'Expand' and form1.validate_on_submit():
+            raw_data_table = df.to_html(classes = 'table table-hover')
+        if request.form['action'] == 'Collapse' and form2.validate_on_submit():
+            raw_data_table = ''
+
+    
+
+    return render_template(
+        'obama.html',
+        title='Obama',
+        year=datetime.now().year,
+        message='Obama dataset page.',
+        img_trump = '/static/imgs/obama.jpg',
+        img_obama = '/static/imgs/obama.jpg',
+        img_bush = '/static/imgs/obama.jpg',
+        img_clinton = '/static/imgs/obama.jpg',
+        raw_data_table = raw_data_table,
+        form1 = form1,
+        form2 = form2
+    )
+
+@app.route('/data/bush' , methods = ['GET' , 'POST'])
+def bush():
+
+    print("Bush")
+
+    """Renders the about page."""
+    form1 = ExpandForm()
+    form2 = CollapseForm()
+    # df = pd.read_csv(path.join(path.dirname(__file__), 'static\\data\\trump.csv'))
+    df = pd.read_csv(path.join(path.dirname(__file__), 'static/data/bush.csv'))
+    raw_data_table = ''
+
+    if request.method == 'POST':
+        if request.form['action'] == 'Expand' and form1.validate_on_submit():
+            raw_data_table = df.to_html(classes = 'table table-hover')
+        if request.form['action'] == 'Collapse' and form2.validate_on_submit():
+            raw_data_table = ''
+
+    
+
+    return render_template(
+        'bush.html',
+        title='Bush',
+        year=datetime.now().year,
+        message='Bush dataset page.',
+        img_trump = '/static/imgs/bush.jpg',
+        img_obama = '/static/imgs/bush.jpg',
+        img_bush = '/static/imgs/bush.jpg',
+        img_clinton = '/static/imgs/bush.jpg',
+        raw_data_table = raw_data_table,
+        form1 = form1,
+        form2 = form2
+    )
+
+@app.route('/data/clinton' , methods = ['GET' , 'POST'])
+def clinton():
+
+    print("Clinton")
+
+    """Renders the about page."""
+    form1 = ExpandForm()
+    form2 = CollapseForm()
+    # df = pd.read_csv(path.join(path.dirname(__file__), 'static\\data\\trump.csv'))
+    df = pd.read_csv(path.join(path.dirname(__file__), 'static/data/clinton.csv'))
+    raw_data_table = ''
+
+    if request.method == 'POST':
+        if request.form['action'] == 'Expand' and form1.validate_on_submit():
+            raw_data_table = df.to_html(classes = 'table table-hover')
+        if request.form['action'] == 'Collapse' and form2.validate_on_submit():
+            raw_data_table = ''
+
+    
+
+    return render_template(
+        'clinton.html',
+        title='Clinton',
+        year=datetime.now().year,
+        message='Clinton dataset page.',
+        img_trump = '/static/imgs/clinton.jpg',
+        img_obama = '/static/imgs/clinton.jpg',
+        img_bush = '/static/imgs/clinton.jpg',
+        img_clinton = '/static/imgs/clinton.jpg',
+        raw_data_table = raw_data_table,
+        form1 = form1,
+        form2 = form2
+    )
+
+@app.route('/data/assignment_5130' , methods = ['GET' , 'POST'])
+def assignment_5130():
+
+    print("5130")
+
+    return render_template(
+        'assignment_5130.html'
     )
 
 @app.route('/tracking_changes')
